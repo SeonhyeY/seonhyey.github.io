d7ad064982ab8384f7047391810cb008a6fdeda0
Seonhye Yoon <124941076+SeonhyeY@users.noreply.github.com>
2023-07-08 21:48:48 -0600

    JavaScript - home

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
+// Adapt the dark style to header when the page scrolls down
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 
-// Scrolling down: adapt the dark style to header
 document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+// Make home gradually trasparent when the page scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
