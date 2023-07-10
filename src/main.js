7877328231a61fef714378871e11707c83a4ca60
Seonhye Yoon <124941076+SeonhyeY@users.noreply.github.com>
2023-07-10 01:32:39 -0600

    JavaScript - files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+'use strict';
 // Adapt the dark style to header when the page scrolls down
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
@@ -28,4 +29,17 @@ document.addEventListener('scroll', () => {
   } else {
     scrollTop.classList.remove('hide');
   }
+});
+
+// Toggle the header nav bar
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// Close the Navbar menu automatically when the menu is clicked
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
 });
