ab43575dfd5e5c465f1a2c39e42f76cde20a920e
SeonhyeY <syoon330@gmail.com>
2023-10-20 13:30:05 -0600

    Fix styling errors, and add Youtube clone coding link in project section

--- src/type.js
+++ src/type.js
@@ -2,13 +2,13 @@
 
 new TypeIt('.home__title--strong', {
   loop: true,
-  speed: 70,
+  speed: 90,
 }) // Junior Developer
   .move(-9)
-  .type(' Front-End ') // Junior Front-End Developer
+  .type(' Front-End Developer') // Junior Front-End Developer
   .pause(1000)
-  .delete(10)
-  .type('Software ')
+  .delete(19)
+  .type('Software Developer')
   .move(null, { to: 'END' })
   .pause(1000)
   .go();
